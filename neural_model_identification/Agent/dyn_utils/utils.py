68b68231803ff02e75a3a53770cbd3d1563be5fa
dema <gg.dema.rm.22@gmail.com>
2024-12-23 16:42:46 +0100

    add utility for normalize data

--- neural_model_identification/Agent/dyn_utils/utils.py
+++ neural_model_identification/Agent/dyn_utils/utils.py
@@ -5,3 +5,23 @@ import numpy as np
 
 def euler_integration(x, x_dot, delta_t):
     return x + x_dot * delta_t
+
+
+def normalize_state(state, x_min, x_max):
+    """
+    Normalize state
+    """
+    state = (((state - x_min) / (x_max - x_min)) - 0.5) * 2
+
+    return state
+
+
+
+
+def denormalize_state(state, x_min, x_max):
+    """
+    Denormalize state
+    """
+    state = ((state / 2) + 0.5) * (x_max - x_min) + x_min
+    return state
+
